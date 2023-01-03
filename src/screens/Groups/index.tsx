02b9b6a42374a34e000320d4dddf7c8d032d27fa
Vitor Nunes <stecks10@gmail.com>
2023-01-03 19:51:33 -0300

    Utilizando Styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Hello RocketSeat</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
