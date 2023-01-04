86a0ff427661dcd0c37effcc941751eb65f38235
Vitor Nunes <stecks10@gmail.com>
2023-01-04 14:35:41 -0300

    component de loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { Groups } from '@screens/Groups';
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme'
 
@@ -10,7 +11,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
