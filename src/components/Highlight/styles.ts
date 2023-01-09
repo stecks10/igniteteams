b39676b9985c5ea5f5e6450439ca47307e5c1ea3
Vitor Nunes <stecks10@gmail.com>
2023-01-09 15:12:22 -0300

    component list empty

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -9,7 +9,7 @@ export const Title = styled.Text`
   text-align: center;
 
   font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   color: ${({ theme }) => theme.COLORS.WHITE};
 `;
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from '@components/Highlight';
 import { useState } from 'react';
 import { Container } from './styles';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Gala da rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -21,6 +22,8 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard title={item} />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma" />}
       />
 
     </Container>
