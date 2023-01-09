799079404522eaab85f6dbaa3642c63069fa5336
Vitor Nunes <stecks10@gmail.com>
2023-01-09 18:58:48 -0300

    Estilizando o Placeholder

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -16,7 +16,7 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Input />
+        <Input placeholder="Nome da turma" />
         <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
