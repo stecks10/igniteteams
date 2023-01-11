dd09a3b1478ef9717085babec79f42fd0f79a96f
Vitor Nunes <stecks10@gmail.com>
2023-01-11 10:25:32 -0300

    extrutura de navegacao

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'react-native';
-import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import {
-  useFonts, Poppins_400Regular
-  , Poppins_700Bold
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
