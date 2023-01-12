c857ece3d33c57deecb1c897f7b2a3f0cb448b8c
Vitor Nunes <stecks10@gmail.com>
2023-01-12 08:58:51 -0300

    implementando o voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,23 @@
 import { BackIcon, Container, Logo, BackButton } from "./styles";
 
 import logoImg from '@assets/logo.png';
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoHome() {
+    navigation.navigate('groups');
+  }
   return (
     <Container>
       {
         showBackButton &&
-        <BackButton>
+        <BackButton onPress={handleGoHome}>
           <BackIcon />
         </BackButton>
       }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,9 +2,15 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +23,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   )
